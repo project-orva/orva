5abccb2818e384ab9248daca79e44eb6ddc92b05
GuyARoss <guyallenross@gmail.com>
2019-08-23 22:04:01 -0400

    init profile service.

---
+++ cmd/profiles/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net"
+	"time"
+
+	grpcProfile "github.com/GuyARoss/project-orva/pkg/grpc-profile"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/keepalive"
+	"google.golang.org/grpc/reflection"
+)
+
+func main() {
+	tcpPort := flag.String("p", "3005", "specified port to start the gRPC server on")
+
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%s", *tcpPort))
+	if err != nil {
+		panic(err)
+	}
+
+	grpcServer := grpc.NewServer(grpc.KeepaliveEnforcementPolicy(keepalive.EnforcementPolicy{
+		PermitWithoutStream: true,
+		MinTime:             1 * time.Minute,
+	}))
+
+	grpcProfile.RegisterGrpcProfileServer(grpcServer, &ServiceRequest{})
+	reflection.Register(grpcServer)
+
+	fmt.Println(fmt.Sprintf("gRPC service started on port %s", *tcpPort))
+	if serveErr := grpcServer.Serve(listener); err != nil {
+		panic(serveErr)
+	}
+
+	grpcServer.Serve(listener)
+}
---
+++ cmd/profiles/service.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"context"
+
+	grpcProfile "github.com/GuyARoss/project-orva/pkg/grpc-profile"
+)
+
+// RetrieveFromId gets a profile based on the provided ID
+func (request *ServiceRequest) RetrieveFromId(ctx context.Context, rofileRequest *grpcProfile.ProfileRequest) (*grpcProfile.Profile, error) {
+	return nil, nil
+}
+
+// UpdateProfile updates a profile
+func (request *ServiceRequest) UpdateProfile(ctx context.Context, profile *grpcProfile.Profile) (*grpcProfile.SuccessResponse, error) {
+	return nil, nil
+}
+
+// CreateProfile creates a new profile
+func (request *ServiceRequest) CreateProfile(ctx context.Context, profile *grpcProfile.Profile) (*grpcProfile.SuccessResponse, error) {
+	return nil, nil
+}
