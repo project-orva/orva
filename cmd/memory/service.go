6729c937c8ab6d2e1f4edba390847416fd6cded3
GuyARoss <guyallenross@gmail.com>
2019-08-27 21:01:56 -0400

    init memory service.

---
+++ cmd/memory/service.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"context"
+
+	grpcMemory "github.com/GuyARoss/project-orva/pkg/grpc-memory"
+)
+
+// GetLatestFromUser gets the latest memory from the desired user
+func (server *ServiceRequest) GetLatestFromUser(ctx context.Context, memoryRequest *grpcMemory.MemoryRequest) (*grpcMemory.MemorySet, error) {
+	return nil, nil
+}
+
+// GetLatestFromDevice gets the latest memory from the desired device
+func (server *ServiceRequest) GetLatestFromDevice(ctx context.Context, memoryRequest *grpcMemory.MemoryRequest) (*grpcMemory.MemorySet, error) {
+	return nil, nil
+}
+
+// CreateMemory creates a new memory instance
+func (server *ServiceRequest) CreateMemory(ctx context.Context, memoryRequest *grpcMemory.MemorySet) (*grpcMemory.MemoryRequest, error) {
+	return nil, nil
+}
