dcf8fa573a177c36fe984cc0f98f5a9dd372673f
GuyARoss <guyallenross@gmail.com>
2019-08-27 21:02:06 -0400

    init core service.

---
+++ cmd/core/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net"
+	"time"
+
+	grpcCore "github.com/GuyARoss/project-orva/pkg/grpc-core"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/keepalive"
+	"google.golang.org/grpc/reflection"
+)
+
+func main() {
+	tcpPort := flag.String("p", "3005", "specified port to start the gRPC server on")
+
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%s", *tcpPort))
+	if err != nil {
+		panic(err)
+	}
+
+	grpcServer := grpc.NewServer(grpc.KeepaliveEnforcementPolicy(keepalive.EnforcementPolicy{
+		PermitWithoutStream: true,
+		MinTime:             1 * time.Minute,
+	}))
+
+	// grpcProfile.RegisterGrpcProfileServer(grpcServer, &ServiceRequest{})
+	grpcCore.RegisterGrpcCoreServer(grpcServer, &ServiceRequest{})
+	reflection.Register(grpcServer)
+
+	fmt.Println(fmt.Sprintf("gRPC service started on port %s", *tcpPort))
+	if serveErr := grpcServer.Serve(listener); err != nil {
+		panic(serveErr)
+	}
+
+	grpcServer.Serve(listener)
+}
---
+++ cmd/core/service.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"context"
+
+	grpcCore "github.com/GuyARoss/project-orva/pkg/grpc-core"
+)
+
+// ProcessStatement processes the inputted statement, outputs a response.
+func (server *ServiceRequest) ProcessStatement(ctx context.Context, req *grpcCore.Request) (*grpcCore.Response, error) {
+	return nil, nil
+}
