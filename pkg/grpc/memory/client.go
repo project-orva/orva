7fa9b8db4d5edbf6fdd005364f064604d66e6dd0
GuyARoss <guyallenross@gmail.com>
2019-08-28 17:29:08 -0400

    added grpc memory client.

---
+++ pkg/grpc/memory/client.go
@@ -0,0 +1,23 @@
+package grpcMemory
+
+import (
+	"time"
+
+	grpc "google.golang.org/grpc"
+	"google.golang.org/grpc/keepalive"
+)
+
+// CreateClientConn creates a new core client connection
+func CreateClientConn(hostAddress string) GrpcMemoryClient {
+	k := keepalive.ClientParameters{
+		Time:                time.Minute * 2,
+		PermitWithoutStream: true,
+	}
+
+	cc, err := grpc.Dial(hostAddress, grpc.WithInsecure(), grpc.WithKeepaliveParams(k))
+	if err != nil {
+		panic(err)
+	}
+
+	return NewGrpcMemoryClient(cc)
+}
