70691f9107e68b277551d4be362c3f61ddaca946
GuyARoss <guyallenross@gmail.com>
2019-08-28 17:30:12 -0400

    created an orva core session context.

---
+++ pkg/orva/session-context.go
@@ -0,0 +1,40 @@
+package orva
+
+// AppliedMessage base message that can be after routines cycle
+type AppliedMessage struct {
+	Statement    string
+	AssignedFrom string
+}
+
+// Input user input for the session
+type Input struct {
+	Message  string
+	DeviceID string
+	UserID   string
+}
+
+// SessionContext ~Core Route Session Context
+type SessionContext struct {
+	AppliedMessages *[]AppliedMessage
+	InitialInput    *Input
+	DeviceAccessLvl AccessType
+	UserAccessLvl   AccessType
+}
+
+// Append session dialog to context
+func (ctx *SessionContext) Append(message string, assignedFrom string) {
+	appLen := len(*ctx.AppliedMessages) + 1
+	newMessages := make([]AppliedMessage, appLen)
+
+	derefMsgs := *ctx.AppliedMessages
+	for i := 0; i < appLen; i++ {
+		newMessages[i] = derefMsgs[i]
+	}
+
+	newMessages[appLen] = AppliedMessage{
+		Statement:    message,
+		AssignedFrom: assignedFrom,
+	}
+
+	ctx.AppliedMessages = &newMessages
+}
