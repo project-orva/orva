ab53669c45ec51a4399c3e7931a05221db641fd4
GuyARoss <guyallenross@gmail.com>
2019-08-31 15:11:40 -0400

    basic logic for skill routine.

---
+++ internal/routines/skill.go
@@ -0,0 +1,27 @@
+package routines
+
+import (
+	grpcSkill "github.com/GuyARoss/project-orva/pkg/grpc/skill"
+	"github.com/GuyARoss/project-orva/pkg/orva"
+)
+
+// SkillRouineHandler handles the skill routine
+func (req *RoutineRequest) SkillRouineHandler(ctx *orva.SessionContext) {
+	skillQuery := &grpcSkill.QueryRequest{
+		Statement: ctx.InitialInput.Message,
+	}
+
+	resp, err := req.SkillClient.QuerySkill(nil, skillQuery)
+
+	if err != nil {
+		return
+	}
+
+	skillResp, skillErr := orva.SkillWrapper(resp.Endpoint, ctx)
+
+	if skillErr != nil {
+		return
+	}
+
+	ctx.Append(skillResp, "skill routine")
+}
