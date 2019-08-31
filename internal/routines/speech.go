7c2514df0539985c296f1ebb5861a8726fa48beb
GuyARoss <guyallenross@gmail.com>
2019-08-30 21:18:45 -0400

    init speech routine.

---
+++ internal/routines/speech.go
@@ -0,0 +1,23 @@
+package routines
+
+import (
+	grpcSpeech "github.com/GuyARoss/project-orva/pkg/grpc/speech"
+
+	"github.com/GuyARoss/project-orva/pkg/orva"
+)
+
+// SpeechHandler handles the speech routine.
+func (req *RoutineRequest) SpeechHandler(ctx *orva.SessionContext) {
+	speechRequest := &grpcSpeech.SpeechRequest{
+		Message: ctx.InitialInput.Message,
+	}
+
+	resp, err := req.SpeechClient.DetermineSpeech(nil, speechRequest)
+	if err != nil {
+		ctx.Append("Having a hard time processing that", "speech handler")
+		return
+	}
+
+	ctx.Append(resp.Message, "speech handler")
+	return
+}
