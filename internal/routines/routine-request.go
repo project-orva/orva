ae071749f5e057e3e064211b19aa41bfd5716efa
GuyARoss <guyallenross@gmail.com>
2019-08-31 15:10:58 -0400

    added speech and skill to routine request.

--- internal/routines/routine-request.go
+++ internal/routines/routine-request.go
@@ -1,7 +1,14 @@
 package routines
 
-import grpcProfile "github.com/GuyARoss/project-orva/pkg/grpc/profile"
+import (
+	grpcProfile "github.com/GuyARoss/project-orva/pkg/grpc/profile"
+	grpcSpeech "github.com/GuyARoss/project-orva/pkg/grpc/speech"
+	grpcSkill "github.com/GuyARoss/project-orva/pkg/grpc/skill"
+) 
 
+// RoutineRequest base request settings for routines 
 type RoutineRequest struct {
 	ProfileClient grpcProfile.GrpcProfileClient
+	SpeechClient grpcSpeech.GrpcSpeechClient
+	SkillClient grpcSkill.GrpcSkillClient
 }
