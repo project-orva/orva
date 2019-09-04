d908f8ec41fbc29aaf1f6a15e40148d4dc22358e
GuyARoss <guyallenross@gmail.com>
2019-09-03 23:12:04 -0400

    added client to speech.

---
+++ pkg/grpc/speech/client.go
@@ -0,0 +1,23 @@
+package grpcSpeech
+
+import (
+	"time"
+
+	grpc "google.golang.org/grpc"
+	"google.golang.org/grpc/keepalive"
+)
+
+// CreateClientConn creates a new core client connection
+func CreateClientConn(hostAddress string) GrpcSpeechClient {
+	k := keepalive.ClientParameters{
+		Time:                time.Minute * 2,
+		PermitWithoutStream: true,
+	}
+
+	cc, err := grpc.Dial(hostAddress, grpc.WithInsecure(), grpc.WithKeepaliveParams(k))
+	if err != nil {
+		panic(err)
+	}
+
+	return NewGrpcSpeechClient(cc)
+}
