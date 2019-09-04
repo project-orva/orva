a682b55d09dfc369a0af68fc51533cab41e89450
GuyARoss <guyallenross@gmail.com>
2019-09-03 23:12:28 -0400

    created init fun for routine request.

--- internal/routines/routine-request.go
+++ internal/routines/routine-request.go
@@ -2,13 +2,21 @@ package routines
 
 import (
 	grpcProfile "github.com/GuyARoss/project-orva/pkg/grpc/profile"
-	grpcSpeech "github.com/GuyARoss/project-orva/pkg/grpc/speech"
 	grpcSkill "github.com/GuyARoss/project-orva/pkg/grpc/skill"
-) 
+	grpcSpeech "github.com/GuyARoss/project-orva/pkg/grpc/speech"
+)
 
-// RoutineRequest base request settings for routines 
+// RoutineRequest base request settings for routines
 type RoutineRequest struct {
 	ProfileClient grpcProfile.GrpcProfileClient
-	SpeechClient grpcSpeech.GrpcSpeechClient
-	SkillClient grpcSkill.GrpcSkillClient
+	SpeechClient  grpcSpeech.GrpcSpeechClient
+	SkillClient   grpcSkill.GrpcSkillClient
+}
+
+func initRequests(profileAddress string, speechAddress string, skillAddress string) *RoutineRequest {
+	return &RoutineRequest{
+		ProfileClient: grpcProfile.CreateClientConn(profileAddress),
+		SpeechClient:  grpcSpeech.CreateClientConn(speechAddress),
+		SkillClient:   grpcSkill.CreateClientConn(skillAddress),
+	}
 }
