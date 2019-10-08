8a6752c359e6ebcdfe7ed806fb70b2efa266b589
GuyARoss <guyallenross@gmail.com>
2019-10-07 21:04:04 -0400

    cleanup.

--- cmd/core/service.go
+++ cmd/core/service.go
@@ -6,6 +6,9 @@ import (
 	grpcCore "github.com/GuyARoss/project-orva/pkg/grpc/core"
 )
 
+type ServiceRequest struct {
+}
+
 // ProcessStatement processes the inputted statement, outputs a response.
 func (server *ServiceRequest) ProcessStatement(ctx context.Context, req *grpcCore.Request) (*grpcCore.Response, error) {
 	return nil, nil
