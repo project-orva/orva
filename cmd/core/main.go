a9de9396c1df47d2ab9800c57789dc7a99f6ae95
GuyARoss <guyallenross@gmail.com>
2020-03-17 16:16:56 -0400

    (core/main) added postgresql support

--- cmd/core/main.go
+++ cmd/core/main.go
@@ -7,6 +7,8 @@ import (
 	"time"
 
 	grpcCore "github.com/GuyARoss/project-orva/pkg/grpc/core"
+	grpcSpeech "github.com/GuyARoss/project-orva/pkg/grpc/speech"
+	"github.com/GuyARoss/project-orva/pkg/pgdb"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/keepalive"
@@ -15,10 +17,22 @@ import (
 
 func main() {
 	tcpPort := flag.String("p", "3005", "specified port to start the gRPC server on")
+	pgAddress := flag.String("pg", "http://localhost:32768", "postgresql database address")
+	speechAdress := flag.String("s", "http://localhost:5521", "speech service address")
 
 	listener, err := net.Listen("tcp", fmt.Sprintf(":%s", *tcpPort))
 	if err != nil {
 		panic(err)
+	}
+
+	rr := &RoutineRequest{
+		SpeechClient: grpcSpeech.CreateClientConn(*speechAdress),
+		PgCreds: &pgdb.DbCreds{ // @@@ nut done, plz fix
+			Host:     *pgAddress,
+			User:     "docker",
+			Password: "docker",
+			Dbname:   "docker",
+		},
 	}
 
 	grpcServer := grpc.NewServer(grpc.KeepaliveEnforcementPolicy(keepalive.EnforcementPolicy{
@@ -27,7 +41,9 @@ func main() {
 	}))
 
 	// grpcProfile.RegisterGrpcProfileServer(grpcServer, &ServiceRequest{})
-	grpcCore.RegisterGrpcCoreServer(grpcServer, &ServiceRequest{})
+	grpcCore.RegisterGrpcCoreServer(grpcServer, &ServiceRequest{
+		RoutineRequest: rr,
+	})
 	reflection.Register(grpcServer)
 
 	fmt.Println(fmt.Sprintf("gRPC service started on port %s", *tcpPort))
